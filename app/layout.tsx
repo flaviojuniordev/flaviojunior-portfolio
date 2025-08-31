35b7e93a9671f6df94bfc72598def777b4a715f1
Flavio Junior <devflaviojunior@gmail.com>
2025-08-31 11:01:04 -0300

    favicon fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
       <body>
         {children}
       </body>
